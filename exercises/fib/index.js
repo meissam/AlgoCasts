5d79f31bed3e58fc3b6659a2ef44b64773afd7b0
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-01-13 23:28:27 +0330

    solve the fib chanllenge

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,14 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fib(n - 2) + fib(n - 1);
+  }
+}
 
 module.exports = fib;
